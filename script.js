1257368134173ca4b5c979f0bb607fb4ecd73db1
FelixS <joaosilveirasantos18827@gmail.com>
2026-03-24 22:42:59 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ function toggleBraganca() {
   const info = document.getElementById("braganca-info");
   info.classList.toggle("show");
 }
+
+// SPLASH SCREEN (1 segundo)
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    const splash = document.getElementById("splash");
+    splash.style.opacity = "0";
+    splash.style.transition = "0.5s";
+
+    setTimeout(() => {
+      splash.style.display = "none";
+    }, 500);
+
+  }, 1000);
+});
